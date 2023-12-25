8493a90f13977b604ebed8e76b8745c62e56ed9d
justus.voss <justus.voss@check24.de>
2023-12-26 00:06:24 +0100

    changed api url

--- Frontend/app/library/ickServices.ts
+++ Frontend/app/library/ickServices.ts
@@ -3,7 +3,7 @@ import axios, { AxiosResponse } from 'axios';
 
 const apiURL = process.env.API_URL
   ? process.env.API_URL
-  : 'https://api.icklist.justusvonvoss.me/';
+  : 'https://api.icklist.justusvonvoss.me';
 
 async function getIcks(): Promise<Ick[]> {
   const response: AxiosResponse<Ick[]> = await axios.get(
