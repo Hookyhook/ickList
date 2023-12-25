1c83c167a69d42d7503eb94d0d105483f0c9b8f4
justus.voss <justus.voss@check24.de>
2023-12-26 00:03:30 +0100

    added url

--- Frontend/app/library/ickServices.ts
+++ Frontend/app/library/ickServices.ts
@@ -3,7 +3,7 @@ import axios, { AxiosResponse } from 'axios';
 
 const apiURL = process.env.API_URL
   ? process.env.API_URL
-  : 'https://codeclub.check24.fun:8080';
+  : 'https://api.icklist.justusvonvoss.me/';
 
 async function getIcks(): Promise<Ick[]> {
   const response: AxiosResponse<Ick[]> = await axios.get(
