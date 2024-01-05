a4a893fadaa16bf1d84e93852b89151fd2e21ba1
justus.voss <justus.voss@check24.de>
2024-01-05 02:02:44 +0100

    Redesigned Create Ick Page

--- Frontend/app/(pages)/createIck/page.tsx
+++ Frontend/app/(pages)/createIck/page.tsx
@@ -48,37 +48,41 @@ export default function CreateIck() {
   }
 
   return (
-    <>
-      <h3 className="font-bold text-xl mb-5">New Ick</h3>
-      <div className="flex flex-col gap-7">
-        <label className="form-control">
-          <div className="label">
-            <span className="label-text">Describe your ick</span>
-          </div>
-          <textarea
-            className="textarea textarea-bordered h-24"
-            placeholder="Ick Description"
-          ></textarea>
-        </label>
-        <select
-          className="select select-bordered w-full max-w-xs"
-          defaultValue={'Which gender has this ick?'}
-        >
-          <option>MALE</option>
-          <option>FEMALE</option>
-          <option>GENERAL</option>
-        </select>
-        <select
-          className="select select-bordered w-full max-w-xs"
-          defaultValue={'What kind of ick is it?'}
-        >
-          <option>ICK</option>
-          <option>REVERSEICK</option>
-        </select>
-        <button className="btn btn-primary" onClick={submitIck}>
-          Submit
-        </button>
+    <div className="flex justify-center items-center h-screen">
+      <div className="w-full mx-auto p-6 bg-blend-darken shadow-lg rounded-lg">
+        <h3 className="font-bold text-3xl mb-5 text-center text-primary">
+          New Ick
+        </h3>
+        <div className="flex flex-col gap-7">
+          <label className="form-control">
+            <div className="label">
+              <span className="label-text">Describe your ick</span>
+            </div>
+            <textarea
+              className="textarea textarea-bordered h-24"
+              placeholder="Ick Description"
+            ></textarea>
+          </label>
+          <select
+            className="select select-bordered w-full max-w-xs"
+            defaultValue={'Which gender has this ick?'}
+          >
+            <option>MALE</option>
+            <option>FEMALE</option>
+            <option>GENERAL</option>
+          </select>
+          <select
+            className="select select-bordered w-full max-w-xs"
+            defaultValue={'What kind of ick is it?'}
+          >
+            <option>ICK</option>
+            <option>REVERSEICK</option>
+          </select>
+          <button className="btn btn-primary" onClick={submitIck}>
+            Submit
+          </button>
+        </div>
       </div>
-    </>
+    </div>
   );
 }
