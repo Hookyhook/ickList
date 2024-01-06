3c7a513dcbaae3908ab0c09843ea84c27ae5b6ac
justus.voss <justus.voss@check24.de>
2024-01-06 16:04:00 +0100

    Redesigned Create Ick Page and Removed User Zooming

--- Frontend/app/(pages)/layout.tsx
+++ Frontend/app/(pages)/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <Navbar></Navbar>
-
+        <meta name="viewport" content="width=device-width, user-scalable=no" />
         {children}
         <NewIckButton></NewIckButton>
       </body>
