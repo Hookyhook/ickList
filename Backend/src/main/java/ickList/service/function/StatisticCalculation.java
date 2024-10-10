9dd11960d60b26a79b0d40a216cbeee52a916f40
Hookyhook <justus.von.voss@gmail.com>
2024-10-10 15:49:19 +0200

    Fixed arithmetic problems

--- Backend/src/main/java/ickList/service/function/StatisticCalculation.java
+++ Backend/src/main/java/ickList/service/function/StatisticCalculation.java
@@ -11,12 +11,15 @@ import java.util.stream.Stream;
 import ickList.model.Ick;
 
 public class StatisticCalculation implements Function<List<Ick>, Integer> {
-	@Override
-	public Integer apply(List<Ick> Icks){
-		LocalDate currenDate = LocalDate.now();
-		Month currentMonth = currenDate.getMonth();
-		int newIcksThisMonth = Icks.stream().filter(ick -> ick.getCreatedOn().atZone(ZoneId.systemDefault()).getMonth() == currentMonth).toArray().length;
-		int oldIcks = Icks.stream().filter(ick -> ick.getCreatedOn().atZone(ZoneId.systemDefault()).getMonth() != currentMonth).toArray().length;
-		return newIcksThisMonth /  oldIcks == 0 ? 100 : oldIcks;
-	}
+
+    @Override
+    public Integer apply(List<Ick> Icks) {
+        LocalDate currenDate = LocalDate.now();
+        Month currentMonth = currenDate.getMonth();
+        int newIcksThisMonth = Icks.stream().filter(ick -> ick.getCreatedOn().atZone(ZoneId.systemDefault()).getMonth() == currentMonth)
+            .toArray().length;
+        int oldIcks = Icks.stream().filter(ick -> ick.getCreatedOn().atZone(ZoneId.systemDefault()).getMonth() != currentMonth)
+            .toArray().length;
+        return newIcksThisMonth / (oldIcks == 0 ? 100 : oldIcks);
+    }
 }
