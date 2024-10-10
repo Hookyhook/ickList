977466530635c5d987d88342e8e07b404b37faa2
Hookyhook <justus.von.voss@gmail.com>
2024-10-10 15:41:11 +0200

    Added StatisticCalculation logic

--- Backend/src/main/java/ickList/service/function/StatisticCalculation.java
+++ Backend/src/main/java/ickList/service/function/StatisticCalculation.java
@@ -1,14 +1,22 @@
 package ickList.service.function;
 
+import java.time.LocalDate;
+import java.time.Month;
+import java.time.ZoneId;
+import java.util.Calendar;
 import java.util.List;
 import java.util.function.Function;
+import java.util.stream.Stream;
 
 import ickList.model.Ick;
 
 public class StatisticCalculation implements Function<List<Ick>, Integer> {
 	@Override
 	public Integer apply(List<Ick> Icks){
-
-		return 1;
+		LocalDate currenDate = LocalDate.now();
+		Month currentMonth = currenDate.getMonth();
+		int newIcksThisMonth = Icks.stream().filter(ick -> ick.getCreatedOn().atZone(ZoneId.systemDefault()).getMonth() == currentMonth).toArray().length;
+		int oldIcks = Icks.stream().filter(ick -> ick.getCreatedOn().atZone(ZoneId.systemDefault()).getMonth() != currentMonth).toArray().length;
+		return newIcksThisMonth /  oldIcks == 0 ? 100 : oldIcks;
 	}
 }
