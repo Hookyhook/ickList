4476ba6cc736e2063a422d94168f005579a924d7
Hookyhook <justus.von.voss@gmail.com>
2025-02-03 12:15:19 +0100

    Downgraded eslint

--- Backend/src/main/java/ickList/rest/IckController.java
+++ Backend/src/main/java/ickList/rest/IckController.java
@@ -13,8 +13,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import ickList.service.IckService;
 import ickList.model.Ick;
+import ickList.service.IckService;
 import lombok.AllArgsConstructor;
 
 @RestController
