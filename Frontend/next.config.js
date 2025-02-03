946a0a1caaa16a552c131f994ada52b5f725b1b6
Hookyhook <justus.von.voss@gmail.com>
2025-02-03 17:16:17 +0100

    Rollback of Tailwind upgrade

--- Frontend/next.config.js
+++ Frontend/next.config.js
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'standalone',
-  experimental: {
-    missingSuspenseWithCSRBailout: false,
-  },
 };
 
 module.exports = nextConfig;
